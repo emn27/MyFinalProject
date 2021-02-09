fda13ed0d75782f1349de6b07c4e6556f5702472
eminebülbül <emn.bulbul@hotmail.com>
2021-02-09 17:36:33 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Category:IEntity
+    {
+        //Çıplak Class Kalmasın eğerki bir class herhangi bir interface implemente almıyorsa giderir.
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
